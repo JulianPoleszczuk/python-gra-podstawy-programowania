2f4c267320d78a79a4a8a496d23c0f0e814abbbd
Julian Poleszczuk <julianpoleszczuk37@gmail.com>
2026-03-05 20:31:21 +0100

    uruchomienie okna

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello world")
+import pygame
+pygame.init()
+width, height = 640,640
+screen = pygame.display.set_mode((width, height))
+dziala = True
+while dziala:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            dziala= False
+pygame.quit()
