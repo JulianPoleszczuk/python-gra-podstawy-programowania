4fa65ed2e0dc00832600a3e7f13b1890f2dda25c
Julian Poleszczuk <julianpoleszczuk37@gmail.com>
2026-03-12 13:03:55 +0100

    w trakcie poprawiania movementu

--- movement.py
+++ movement.py
@@ -1,20 +1,11 @@
 import pygame
-import time
+pygame.init()
+postacie =pygame.image.load("assets/tux.png").convert_alpha()
+wysokosc_1 = 32
+szerokosc_1 = 32
+pingwin_biegajacy = postacie.subsurface((0,0, szerokosc_1, wysokosc_1))
 def ruszanie(gracz):
         klawisz = pygame.key.get_pressed()
-        if klawisz[pygame.K_LEFT]:
-            gracz.x -= gracz.pred
-            if gracz.pred > 8:
-                gracz.pred += 0.2
-        if klawisz[pygame.K_RIGHT]:
-            gracz.x += gracz.pred
-            if gracz.pred > 8:
-                gracz.pred += 0.2
-        if klawisz[pygame.K_UP]:
-                if gracz.pred > 8:
-                    gracz.pred += 0.2
-        gracz.y -= gracz.pred
-        if klawisz[pygame.K_DOWN]:
-                if gracz.pred > 8:
-                    gracz.pred += 0.2
-        gracz.y += gracz.pred
+        if klawisz[pygame.K_LSHIFT]:
+            pass
+
